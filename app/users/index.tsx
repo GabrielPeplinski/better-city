7cd27a79b0118642f4caf6fa50276218cff4a767
GabrielPeplinski <gabrielspeplinski2301@gmail.com>
2023-03-22 00:22:44 -0300

    Roteamento adicionado

--- app/users/index.tsx
+++ app/users/index.tsx
@@ -1,14 +1,11 @@
 import { Text, View } from 'react-native'
 import React, { Component } from 'react'
-import { Link } from 'expo-router'
 
 export default class index extends Component {
   render() {
     return (
       <View>
-        <Text>Login Page</Text>
-        <Text></Text>
-        <Link href='/users'>Go to users</Link>
+        <Text>Create User Page</Text>
       </View>
     )
   }
